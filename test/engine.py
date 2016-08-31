b00035ee38ead0e4dc56b1e640fb0ae8962dd90a
John M Calhoun <johnmcalhoun123@gmail.com>
2016-08-31 17:15:05 -0500

    basic unittest classes

---
+++ test/engine.py
@@ -0,0 +1,12 @@
+import unittest
+import engine
+
+class engine_test(unittest.TestCase):
+    def test_load(self):
+        pass    
+
+    def test_render(self):
+        pass 
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/steps.py
@@ -0,0 +1,18 @@
+import unittest
+from steps import *
+
+class steps_test(unittest.TestCase):
+    def test_pandoc(self):
+        pass    
+
+    def test_shell(self):
+        pass    
+
+    def test_knitr(self):
+        pass    
+
+    def test_markdown(self):
+        pass 
+
+if __name__ == '__main__':
+    unittest.main()
